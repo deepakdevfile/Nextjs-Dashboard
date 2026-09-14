7dfce7c4cefe38734b3f004f9f7fddbfe54e0352
deepakdevfile <deepakdevfile@gmail.com>
2026-09-14 17:32:38 +0530

    C12: Handling Errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -150,10 +150,10 @@ export async function fetchInvoiceById(id: string) {
 
     const invoice = data.map((invoice) => ({
       ...invoice,
-      // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
